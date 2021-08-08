62669f2948a72d5b8e1157026ee99cd008c2e428
Ricardo Ospina <ricardo@MacBook-Pro-de-Ricardo.local>
2021-08-07 19:37:36 -0500

    Create Runner.java

---
+++ src/test/java/com/automation/runners/Runner.java
@@ -0,0 +1,18 @@
+package com.automation.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        tags = {"@TestCase_8"},
+        glue = {"com.automation.definitions"},
+        snippets = SnippetType.CAMELCASE,
+        monochrome = true,
+        dryRun = false
+)
+public class Runner {
+}
